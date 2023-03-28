176b2c9838ff9b84ebc17ceef8bd95b307a6c562
ErikJava <zycore90@gmail.com>
2023-03-28 17:50:55 +0200

    Added menu, made JSON-Strings appear on one line.

--- src/main/java/com/springboot/springboot/PersonController.java
+++ src/main/java/com/springboot/springboot/PersonController.java
@@ -1,5 +1,6 @@
 package com.springboot.springboot;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.aot.hint.TypeReference;
@@ -14,28 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
     @GetMapping("/json")
-    public ResponseEntity<String> getJsonString(@RequestParam String objName, @RequestParam String attributeName,
-                                                @RequestParam(required = false, defaultValue = "false") boolean receiveWholeObject,
+    public ResponseEntity<String> getJsonString(@RequestParam(required = false) String objName,
                                                 @RequestParam(required = false, defaultValue = "false") boolean retrieveWholeFile) throws IOException {
         InputStream inputStream = TypeReference.class.getResourceAsStream("/json/data.json");
         String jsonString = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
         if (retrieveWholeFile) {
-            return ResponseEntity.ok().body(jsonString);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(JsonGenerator.Feature.QUOTE_FIELD_NAMES, true);
+            return ResponseEntity.ok().body(mapper.writeValueAsString(mapper.readValue(jsonString, Object.class)));
+        }
+        if (objName == null || objName.isEmpty()) {
+            return ResponseEntity.badRequest().build();
         }
         JsonNode rootNode = new ObjectMapper().readTree(jsonString);
         JsonNode objNode = rootNode.get(objName);
         if (objNode == null) {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.ok().body("null");
         }
-        if (receiveWholeObject) {
-            String json = new ObjectMapper().writeValueAsString(objNode);
-            return ResponseEntity.ok().body(json);
-        }
-        JsonNode attrNode = objNode.get(attributeName);
-        if (attrNode == null) {
-            return ResponseEntity.notFound().build();
-        }
-        String json = new ObjectMapper().writeValueAsString(attrNode);
+        String json = new ObjectMapper().writeValueAsString(objNode);
         return ResponseEntity.ok().body(json);
     }
 }
+
+
+
