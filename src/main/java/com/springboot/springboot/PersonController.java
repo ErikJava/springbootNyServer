d2034964d1843d97f039df1ab17d9d830cfdf54f
ErikJava <zycore90@gmail.com>
2023-03-31 12:28:42 +0200

    Added comments.

--- src/main/java/com/springboot/springboot/PersonController.java
+++ src/main/java/com/springboot/springboot/PersonController.java
@@ -14,10 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
+
+    // This maps an HTTP GET request with the path "/json" to the getJsonString method.
     @GetMapping("/json")
+    //  This is the method signature. It takes two parameters, objName and retrieveWholeFile, both of which are optional query parameters.
     public ResponseEntity<String> getJsonString(@RequestParam(required = false) String objName,
                                                 @RequestParam(required = false, defaultValue = "false") boolean retrieveWholeFile) throws IOException {
+        // Read the data.json file as an InputStream
         InputStream inputStream = TypeReference.class.getResourceAsStream("/json/data.json");
+
+        // Convert the InputStream to a String
         String jsonString = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
         if (retrieveWholeFile) {
             ObjectMapper mapper = new ObjectMapper();
@@ -27,11 +33,15 @@ public class PersonController {
         if (objName == null || objName.isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
+
+        // Parse the JSON string and retrieve the JSON object specified by objName
         JsonNode rootNode = new ObjectMapper().readTree(jsonString);
         JsonNode objNode = rootNode.get(objName);
         if (objNode == null) {
             return ResponseEntity.ok().body("null");
         }
+
+        // Convert the JSON object to a JSON string and return a response with the body containing the string
         String json = new ObjectMapper().writeValueAsString(objNode);
         return ResponseEntity.ok().body(json);
     }
